f02a7468a4f40d07fac7ca535cac21325728d760
DaMVgt <dmonterroso098@outlook.com>
2022-04-15 17:58:00 -0600

    Aplicativo Producer Demo

---
+++ Model/Aspirante.cs
@@ -0,0 +1,11 @@
+namespace ProducerDemo.Model
+{
+    public class Aspirante
+    {
+        public string NoExpediente { get; set; }
+        public string Apellidos { get; set; }
+        public string Nombres { get; set; }
+        public string Direccion { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+using System.Text.Json;
+using ProducerDemo.Model;
+using RabbitMQ.Client;
+
+public class program
+{
+    public static void Main(string[] args)
+    {
+        IConnection conexion = null;
+        IModel channel = null;
+        var connectionFactory = new ConnectionFactory { HostName = "localhost", VirtualHost = "/", Port = 5672, UserName = "guest", Password = "guest" };
+        conexion = connectionFactory.CreateConnection();
+        channel = conexion.CreateModel();
+        
+        Aspirante registro = new Aspirante();
+        registro.NoExpediente = "EXP-2022001";
+        registro.Apellidos = "Mancilla Paz";
+        registro.Nombres = "Yolanda";
+        registro.Direccion = "Guatemala, Guatemala";
+        registro.Email = "ymancilla@gmail.com";
+
+        channel.BasicPublish("exchange-demo", "", null, Encoding.UTF8.GetBytes(JsonSerializer.Serialize(registro)));
+        
+        Console.WriteLine("Mensaje enviado, presione una tecla para terminar...");
+        Console.ReadLine();
+    }
+}
